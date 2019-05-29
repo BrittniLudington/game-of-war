b804efae53d97e941895cd50102c454d18324ecf
BrittniLudington <brittniludington@gmail.com>
2019-05-29 16:20:34 -0700

    console logging

--- src/components/gamecomponents/currentGameData.js
+++ src/components/gamecomponents/currentGameData.js
@@ -87,7 +87,7 @@ export default class dataProvider extends React.Component
     componentWillMount()
     {
         console.log(this.props);
-        const username = this.props._owner.pendingProps.match.params.username;//this.props.children._self.props.match.params.username;
+        const username = this.props.children._owner.pendingProps.match.params.username;//this.props.children._self.props.match.params.username;
         fetch(`https://game-of-war-server.herokuapp.com/games/${username}`,
         {
             headers: {'Content-Type':'application/json'}
